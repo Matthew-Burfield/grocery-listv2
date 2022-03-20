6e7ce80b1b3989371e3cb6f2fce9763073748fdd
Matthew Burfield <m1.burfield@gmail.com>
2022-03-21 07:44:46 +1000

    refactor: eslint warnings

--- app/routes/notes/$noteId.tsx
+++ app/routes/notes/$noteId.tsx
@@ -1,39 +1,40 @@
-import type { LoaderFunction, ActionFunction } from "remix";
-import { redirect } from "remix";
-import { json, useLoaderData, useCatch, Form } from "remix";
-import invariant from "tiny-invariant";
+// import type { LoaderFunction, ActionFunction } from "remix";
+// import { redirect } from "remix";
+// import { json, useLoaderData, useCatch, Form } from "remix";
+import { useCatch, Form } from "remix";
+// import invariant from "tiny-invariant";
 // import type { Note } from "~/models/note.server";
 // import { deleteNote } from "~/models/note.server";
 // import { getNote } from "~/models/note.server";
-import { requireUserId } from "~/session.server";
+// import { requireUserId } from "~/session.server";
 
-type LoaderData = {
-  // note: Note;
-};
+// type LoaderData = {
+//   note: Note;
+// };
 
-export const loader: LoaderFunction = async ({ request, params }) => {
-  const userId = await requireUserId(request);
-  invariant(params.noteId, "noteId not found");
+// export const loader: LoaderFunction = async ({ request, params }) => {
+//   const userId = await requireUserId(request);
+//   invariant(params.noteId, "noteId not found");
+//
+//   const note = await getNote({ userId, id: params.noteId });
+//   if (!note) {
+//     throw new Response("Not Found", { status: 404 });
+//   }
+//   return json<LoaderData>({ note });
+//   return json({ success: true });
+// };
 
-  // const note = await getNote({ userId, id: params.noteId });
-  // if (!note) {
-  //   throw new Response("Not Found", { status: 404 });
-  // }
-  // return json<LoaderData>({ note });
-  return json({ success: true });
-};
-
-export const action: ActionFunction = async ({ request, params }) => {
-  const userId = await requireUserId(request);
-  invariant(params.noteId, "noteId not found");
-
-  // await deleteNote({ userId, id: params.noteId });
-
-  return redirect("/notes");
-};
+// export const action: ActionFunction = async ({ request, params }) => {
+//   const userId = await requireUserId(request);
+//   invariant(params.noteId, "noteId not found");
+//
+//   // await deleteNote({ userId, id: params.noteId });
+//
+//   return redirect("/notes");
+// };
 
 export default function NoteDetailsPage() {
-  const data = useLoaderData() as LoaderData;
+  // const data = useLoaderData() as LoaderData;
 
   return (
     <div>
--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { Form, json, redirect, useActionData } from "remix";
+import { Form, json, /* redirect, */ useActionData } from "remix";
 import type { ActionFunction } from "remix";
 import Alert from "@reach/alert";
 
 // import { createNote } from "~/models/note.server";
-import { requireUserId } from "~/session.server";
+// import { requireUserId } from "~/session.server";
 
 type ActionData = {
   errors?: {
@@ -14,7 +14,7 @@ type ActionData = {
 };
 
 export const action: ActionFunction = async ({ request }) => {
-  const userId = await requireUserId(request);
+  // const userId = await requireUserId(request);
 
   const formData = await request.formData();
   const title = formData.get("title");
